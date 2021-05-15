8aa3cac7d2f35c391455896220dfc805f7942e90
mustafa abdishakur <mx_1200@hotmail.com>
2021-05-15 20:40:18 +0400

    adding elements and css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import profilePic from './img/blank-profile.png';
 import optionsImg from './img/options.png';
-
+import emojiBtn from './img/emoji-btn.png';
 function App() {
   return (
     <div className="App">
@@ -10,7 +10,7 @@ function App() {
          <div className="main-profile-container">
             {/* profile pic, name, create group, sign out, optional: name and username instead of just name */}
             <div className="profile-info-container">
-              <img src={profilePic} alt="profile picture" />
+              <img src={profilePic} alt="profile" />
               <span>Name</span>
             </div>
             <div className="profile-options-container">
@@ -18,8 +18,29 @@ function App() {
             </div>
             
          </div>
+         <div className="group-list-container">
+           <ul>
+             <li>
+               <div className="list-item">
+                 Demo group
+               </div>
+             </li>
+           </ul>
+         </div>
        </div>
-       <div className="middle-container"></div>
+       <div className="middle-container">
+         <div className="group-info-container">
+           <h1>Demo group</h1>
+         </div>
+          <div className="main-chat-container">
+
+          </div>
+          <div className="main-text-container">
+            <input type="text" />
+            <img src={emojiBtn} alt="emoji" />
+          </div>
+       
+       </div>
        <div className="right-container"></div>
      </div>
     </div>
